3b928f43fef42d52edfc98aa2acdb3a0d53dc4ce
Rahul <123063375+RahulCSS@users.noreply.github.com>
2025-03-04 10:27:53 +0530

    Adding dotenv

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,17 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
+import Home from "./pages/home"
+import About from "./pages/about"
+import Services from "./pages/services"
 
 const App = () => {
   return (
-    <div>App</div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/services" element={<Services />} />
+      </Routes>
+    </Router>
   )
 }
 
